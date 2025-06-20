65f592b4eb3044831f8e9d19adef700c9341d811
Ana Braghim <anajuliabraghim@gmail.com>
2025-06-20 19:02:17 -0300

    fix: prismaclient in db

--- src/db.js
+++ src/db.js
@@ -1,2 +1,3 @@
-import { PrismaClient } from "../generated/prisma/index.js";
+//import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaClient } from '@prisma/client';
 export const prisma = new PrismaClient()
